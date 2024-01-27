3920f45bdcef3281c1cd8cad6cf0022547b9fcde
Vinay Maggidi <111217552+victoryanonymous@users.noreply.github.com>
2024-01-27 18:57:11 +0530

    Initial Routes

---
+++ server.js
@@ -0,0 +1,28 @@
+const express =require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const indexRouter = require('./routes/index');
+const authorsRouter = require('./routes/authors')
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/testDb')
+.then(()=> console.log("Connected"))
+.catch(()=> console.log("Error"));
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
+app.use(expressLayouts);
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
+app.use(express.static('public'));
+app.use('/', indexRouter);
+app.use('/authors', authorsRouter);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port,() => {
+    console.log(`Server is Running on ${port}`);
+})
